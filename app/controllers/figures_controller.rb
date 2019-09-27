7a11cadd2da246dd659cddd8275e14ae25c389b9
Sara Harvey <saraharveynyc@gmail.com>
2019-09-27 17:14:01 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -28,5 +28,23 @@ class FiguresController < ApplicationController
     @figure = Figure.find(params[:id])
     erb :'figures/show'
   end
+  
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 
 end
